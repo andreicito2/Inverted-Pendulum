d0dd2e76e37f80d2e2fb9838e3370c0bf0b85e72
avmai <avmaiorov@hotmail.com>
2025-06-11 09:26:15 -0700

    Final code

--- Control Code/ctrlbox.m
+++ Control Code/ctrlbox.m
@@ -8,6 +8,9 @@
 %
 function rval = ctrlbox_init()
     global ctrlbox_con;
+    global ctrlbox_err;
+
+    ctrlbox_err = 0;
 
     ctrlbox_con = socket();
     sinfo = struct("addr","169.254.0.100", "port", 47820);
@@ -26,10 +29,15 @@ endfunction
 %    if excessive.
 function rval = ctrlbox_send(cmdval,enable,period)
     global ctrlbox_con;
+    global ctrlbox_err;
 
     pwm = min(max(cmdval,-32000),32000);
     data = [pwm, 0, enable, period];
-    send(ctrlbox_con, typecast(int32(data(1:4)),'uint8'));
+    try
+        send(ctrlbox_con, typecast(int32(data(1:4)),'uint8'));
+    catch
+        ctrlbox_err = 1;
+    end_try_catch
 
     rval = 0;
     return;
@@ -39,11 +47,15 @@ endfunction
 %
 function data = ctrlbox_recv()
     global ctrlbox_con;
+    global ctrlbox_err;
 
     [rdata,len] = recv(ctrlbox_con,16);
 
     if (len ~= 16)
         fprintf('short data: %d\n', len);
+        data = [0,0,0,0];
+        ctrlbox_err = 2;
+        return;
     end
 
     data = double(typecast(rdata,'int32'));
@@ -52,13 +64,28 @@ function data = ctrlbox_recv()
 endfunction
 
 %
+% ctrlbox_error - check for a comm error
+%
+function val = ctrlbox_error()
+    global ctrlbox_err;
+
+    val = ctrlbox_err;
+
+    return;
+endfunction
+
+%
 % ctrlbox_shutdown - shutdown connection to ctrlbox
 %
 function ctrlbox_shutdown()
     global ctrlbox_con;
+    global ctrlbox_err;
+
+    ctrlbox_err = 0;
 
     % turn off motor
     send(ctrlbox_con,typecast(int32([0,0,0,0]),'uint8'));
 
     disconnect(ctrlbox_con);
+    close(ctrlbox_con);
 endfunction
